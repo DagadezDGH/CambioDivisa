1448c814c11e1af6bb345d0f00375f91f5dc38fb
DagadezDGH <davidgarridohdez@gmail.com>
2020-10-06 09:17:05 +0100

    Termiando

--- src/main/java/dad/javafx/cambiodivisa/CambioDivisa.java
+++ src/main/java/dad/javafx/cambiodivisa/CambioDivisa.java
@@ -1,6 +1,7 @@
 package dad.javafx.cambiodivisa;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,17 +14,24 @@ import javafx.stage.Stage;
 public class CambioDivisa extends Application {
 	private TextField text1;
 	private TextField text2;
-	private ComboBox combo1;
-	private ComboBox combo2;
+	private ComboBox<Divisa> combo1;
+	private ComboBox<Divisa> combo2;
 	private Button cambiarButton;
-	
+	private Divisa euro = new Divisa("Euro", 1.0);
+	private Divisa libra = new Divisa("Libra", 0.8873);
+	private Divisa dolar = new Divisa("Dolar", 1.2007);
+	private Divisa yen = new Divisa("Yen", 133.59);
+	private Divisa [] divisas = {euro, libra, dolar, yen};
+	   
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		text1 = new TextField("0");
 		text1.setPrefColumnCount(4);
 
-		combo1 = new ComboBox();
+		combo1 = new ComboBox<>();
+		combo1.getItems().addAll(divisas);
+		combo1.getSelectionModel().select(euro);
 		
 		HBox hbox1 = new HBox();
 		hbox1.setAlignment(Pos.BASELINE_CENTER);
@@ -32,22 +40,25 @@ public class CambioDivisa extends Application {
 		
 		text2 = new TextField("0");
 		text2.setPrefColumnCount(4);
+		text2.setEditable(false);
 		
-		combo2 = new ComboBox();
-
+		combo2 = new ComboBox<>();
+		combo2.getItems().addAll(divisas);
+		combo2.getSelectionModel().select(libra);
+		
 		HBox hbox2 = new HBox();
 		hbox2.setAlignment(Pos.BASELINE_CENTER);
 		hbox2.setSpacing(5);
 		hbox2.getChildren().addAll(text2, combo2);
 		
 		cambiarButton = new Button("Cambiar");
-		
+		cambiarButton.setOnAction(e-> onCambiarAction(e));
 		VBox root = new VBox();
 		root.setSpacing(5);
 		root.setAlignment(Pos.CENTER);
 		root.getChildren().addAll(hbox1, hbox2, cambiarButton);
 		
-		Scene scene = new Scene(root, 320, 200);
+		Scene scene = new Scene(root, 420, 200);
 		
 		primaryStage.setTitle("CambioDivisa");
 		primaryStage.setScene(scene);
@@ -55,7 +66,16 @@ public class CambioDivisa extends Application {
 		
 	}
 
-		   public static void main(String args[]){ 
+		   private Object onCambiarAction(ActionEvent e) {
+	       double cantidad1 = Double.parseDouble(text1.getText());
+	       Divisa divisa1 = combo1.getSelectionModel().getSelectedItem();
+	       Divisa divisa2 =combo2.getSelectionModel().getSelectedItem();
+	       double cantidad2 = divisa2.fromEuro(divisa1.toEuro(cantidad1));
+	       text2.setText(""+ cantidad2);
+		return null;
+	}
+
+		public static void main(String args[]){ 
 		      launch(args); 
 		   } 
 }
